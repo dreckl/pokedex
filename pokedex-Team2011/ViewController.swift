3013b40f64c9e0e1e269368ccb127bf842a338f1
dreckl <Roland.Eckl@gmx.net>
2016-01-13 21:14:52 +0100

    Initial Commit

---
+++ pokedex-Team2011/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-Team2011
+//
+//  Created by Eckl on 13.01.16.
+//  Copyright © 2016 Team2011.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
