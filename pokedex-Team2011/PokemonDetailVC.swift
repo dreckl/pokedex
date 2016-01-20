00425b399b2b982f530087bee11789d110a781d9
dreckl <Roland.Eckl@gmx.net>
2016-01-20 18:17:09 +0100

    Stackview

--- pokedex-Team2011/PokemonDetailVC.swift
+++ pokedex-Team2011/PokemonDetailVC.swift
@@ -11,6 +11,21 @@ import UIKit
 class PokemonDetailVC: UIViewController {
     
     @IBOutlet var nameLbl: UILabel!
+    @IBOutlet var mainImg: UIImageView!
+    @IBOutlet var descriptionLbl: UILabel!
+    @IBOutlet var typeLbl: UILabel!
+    @IBOutlet var defenseLbl: UILabel!
+    @IBOutlet var heightLbl: UILabel!
+    @IBOutlet var pokedexLbl: UILabel!
+    @IBOutlet var weightLbl: UILabel!
+    @IBOutlet var attckLbl: UILabel!
+    @IBOutlet var currentEvoImg: UIImageView!
+    @IBOutlet var nextEvoImg: UIImageView!
+    @IBOutlet var evoLbl: UILabel!
+
+    
+    
+    
     var pokemon: Pokemon!
 
     override func viewDidLoad() {
@@ -25,15 +40,10 @@ class PokemonDetailVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    */
 
+    
 }
